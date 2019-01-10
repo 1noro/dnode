36e3660e78c71eb7b3c60cb22839c87ea4a57b19
boot1110001 <boot111.0001@gmail.com>
2019-01-10 21:54:26 +0100

    add multiconn structure

---
+++ tests/test1-monoconn/cli.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#cli
+#by boot1110001
+
+### IMPORTS ####################################################################
+import sys
+import socket
+
+### NON EDITABLE VARIABLES #####################################################
+
+
+### EDITABLE VARIABLES #########################################################
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65433        # Port to listen on (non-privileged ports are > 1023)
+
+### FUNCTIONS ##################################################################
+
+
+### MAIN #######################################################################
+def main(argv):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while True:
+            mystr=input('[<--] To send:  ')
+            s.sendall(str.encode(mystr))
+            mybytes = s.recv(1024)
+            responsestr=mybytes.decode()
+            print('[-->] Received: '+responsestr)
+
+### EXEC #######################################################################
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- tests/test1-monoconn/srv.py
+++ tests/test1-monoconn/srv.py
@@ -11,7 +11,7 @@ import socket
 
 ### EDITABLE VARIABLES #########################################################
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 65433        # Port to listen on (non-privileged ports are > 1023)
 
 ### FUNCTIONS ##################################################################
 
