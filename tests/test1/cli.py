bdedcbc96163a5033a0c40d322bb75e41684fc8e
boot1110001 <boot111.0001@gmail.com>
2019-01-10 21:37:30 +0100

    paso1

--- tests/test1/cli.py
+++ tests/test1/cli.py
@@ -20,11 +20,14 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 def main(argv):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
+        mystr=input('Text to send: ')
+        # s.sendall(b'Hello, world')
+        s.sendall(str.encode(mystr))
+        mybytes = s.recv(1024)
+        responsestr=mybytes.decode()
 
     # print('Received', repr(data))
-    print('Received: '+data.decode("utf-8"))
+    print('Text received: '+responsestr)
 
 ### EXEC #######################################################################
 if __name__ == "__main__":
